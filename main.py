327fd467de8a2404c7d971606ba6c1ef94c7a4a8
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-08 17:48:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from tokenize import endpats
+
+print('89+12-1',89+12-1)
+
+# 1st program
+
+print(9**0.5*5)
+
+# 2st program
+
+print(9.99>9.98 and 1000 != 1000.1)
+
+# 3st program
+
+
+print('2*2+2', 2*2+2)
+print('2*(2+2)', 2*(2+2))
+print('2*2+2, 2*(2+2)', 2*2+2 == 2*(2+2))
+
+
+# 4th program
+
+print('123.456', int(123.456*10%10))
