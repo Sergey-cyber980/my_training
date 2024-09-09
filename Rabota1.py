2acb1a04ed9912d29a57d97d6427643b653633ec
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-09 15:03:30 +0300

    Add files via upload

---
+++ Rabota1.py
@@ -0,0 +1,17 @@
+from tkinter.font import names
+
+example="Топинамбур"
+print(example[0])
+print(example[-1])
+print(example[5:10])
+print(example[::-1])
+print(example[1::2])
+print("*******************")
+# ещё одно длинное слово
+
+a="Производительность"
+print(a[0])
+print(a[-1])
+print(a[9:])
+print(a[::-1])
+print(a[1::3])
