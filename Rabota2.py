241ed6907277016498045d4411ed66e9b7f87bea
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-10 19:45:43 +0300

    Практическое задание по теме "Переменные".

---
+++ Rabota2.py
@@ -0,0 +1,8 @@
+kollichestvo_zadanii = 12
+kollichestvo_vremia = 1.5
+nazvanie_kursa='Python'
+rechenie = (kollichestvo_vremia / kollichestvo_zadanii)
+print(nazvanie_kursa, 'всего задач:',kollichestvo_zadanii,', затрачено часов:'
+      ,kollichestvo_vremia,',', 'среднее время выполнения',rechenie)
+
+# Делал после работы с запятыми не в притык не становяться. Я 2 способа использовал
