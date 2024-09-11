3bd86d48d20c9053cfc27631e5f133b8293d4a27
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-11 22:14:55 +0300

    Add files via upload

---
+++ Rabota3.py
@@ -0,0 +1,14 @@
+# модуль module_1_3.py
+from operator import truediv
+
+name='Сергеев Сергей'
+print('Name:', name)
+
+age=43
+print('Age:', age)
+
+age+=1
+print('New Age:', age)
+
+is_student=True
+print('Is Student:', is_student)
