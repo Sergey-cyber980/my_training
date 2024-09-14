9f179a4661f68c1f6e087c48be1ee90c8ba8ec85
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-14 17:16:51 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,9 @@
+immutable_var = 1, 2, 3, 5, True, 'Stroka'
+print(immutable_var)
+#В закоментированных ниже строках кода указан пример того что зэлементы в кортеже не меняються.
+# immutable_var_ [1] = 23)
+# print(immutable_var_)
+mutable_list = ["Лада приора", "Mesedes", "Porshe"]
+print(mutable_list)
+mutable_list [0] = "Volcvagen"
+print(mutable_list)
