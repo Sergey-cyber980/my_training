2b4b749764c2778bc26a11d403a9095ba7da2b24
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-16 14:06:29 +0300

    Update module_1_7.py

--- module_1_7.py
+++ module_1_7.py
@@ -1,7 +1,9 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
+students = sorted(students)
 def average(grades):
     obchaya_summa = sum(grades)
     return obchaya_summa / len(grades) if len(grades) else 0
 result = {student: average(grade) for student, grade in zip(students, grades)}
 print(result)
+print('***Добавил сортироавку***')
