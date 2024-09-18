a363e973807f908798b5dc7042529af5f1a7e8ad
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-18 22:43:34 +0300

    Условная конструкция. Операторы if, elif, else.

--- module_2_2.py
+++ module_2_2.py
@@ -2,9 +2,9 @@ first = int(input("Введите первое число: "))
 second = int(input("Введите второе число: "))
 third = int(input("Введите третье число: "))
 
-if first == second or first == third or second == third:
-    print("Вывод на консоль число: 2")
-elif first == second == third:
+if first == second == third:
     print("Вывод на консоль число: 3")
+elif first == second or first == third or second == third:
+    print("Вывод на консоль число: 2")
 else:
     print("Вывод на консоль число: 0")
