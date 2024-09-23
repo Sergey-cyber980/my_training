8099ebfe3486559c2c371926e1d96613c4db6abd
Sergey-cyber980 <stalker_angel@mail.ru>
2024-09-23 09:51:07 +0300

    П/з по модулю: "Базовые структуры данных."

--- module_1_7.py
+++ module_1_7.py
@@ -1,6 +1,7 @@
 grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
 students = {'Johnny', 'Bilbo', 'Steve', 'Khendrik', 'Aaron'}
 averagescores = {}
+
 names = list(students)
 names.sort()
 averagescores.update({names[0]:sum(grades[0])/len(grades[0]), names[1]:sum(grades[1])/len(grades[1])})
