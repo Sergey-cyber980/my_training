d1c1d4458f6bd7553c1a36f3447c9366f357156d
Sergey-cyber980 <stalker_angel@mail.ru>
2024-10-06 11:28:20 +0300

    П/з по модулю: "Подробнее о функциях."

--- module_3_hard.py
+++ module_3_hard.py
@@ -1,33 +1,3 @@
-# def calculate_structure_sum(data_structure):
-#     total_sum = 0
-#
-#     for item in data_structure:
-#         if isinstance(item, int):
-#             total_sum += item
-#         elif isinstance(item, str):
-#             total_sum += len(item)
-#         elif isinstance(item, dict):
-#             total_sum += sum(calculate_structure_sum([key, value]) for key, value in item.items())
-#         elif isinstance(item, tuple):
-#             total_sum += sum(calculate_structure_sum([sub_item]) for sub_item in item)
-#         elif isinstance(item, list):
-#             total_sum += calculate_structure_sum(item)
-#
-#     return total_sum
-#
-#
-# data_structure = [
-#     [1, 2, 3],
-#     {'a': 4, 'b': 5},
-#     (6, {'cube': 7, 'drum': 8}),
-#     "Hello",
-#     ((), [{(2, 'Urban', ('Urban2', 35))})
-# ]
-#
-# result = calculate_structure_sum(data_structure)
-# print(result)
-#
-
 def calculate_structure_sum(data_structure):
     total_sum = 0
 
