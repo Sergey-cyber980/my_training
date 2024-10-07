a78ece013cd2877447f61955836cee3e8fe99fbc
Sergey-cyber980 <stalker_angel@mail.ru>
2024-10-07 16:52:27 +0300

    Д/р по уроку "Модули и пакеты"

---
+++ module_4_1.py
@@ -0,0 +1,12 @@
+import fake_math as fd
+import true_math as td
+
+result1 = fd.divide(69, 3)
+result2 = fd.divide(3, 0)
+result3 = td.divide(49, 7)
+result4 = td.divide(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
+
