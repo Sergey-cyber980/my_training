ee273be599e1db8b2ba73686c3f585931bf4db89
Sergey-cyber980 <stalker_angel@mail.ru>
2024-11-11 14:11:16 +0300

    Д/з по теме "Оператор "with".

--- module_7_3.py
+++ module_7_3.py
@@ -5,7 +5,7 @@ class WordsFinder:
     def get_all_words(self):
         all_words = {}
         for file_name in self.file_names:
-            if file_name == file_name:
+            if file_name == file_name: 
                 with open(file_name, 'r', encoding='utf-8') as file:  # Читаем все строки из файла
                     content = file.read().lower()  # Приводим к нижнему регистру
                     content = content.translate([',', '.', '=', '!', '?', ';', ':', ' - '])  # Убираем пунктуацию
@@ -44,4 +44,5 @@ if __name__ == "__main__":
     print(finder2.get_all_words())  # Все слова
     print(finder2.find('TEXT'))  # 3 слово по счёту
     print(finder2.count('teXT'))  # 4 слова teXT в тексте всего
+
 
