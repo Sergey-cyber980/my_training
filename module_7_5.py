ab3c85ea159ed66d0d91b41e484cdad9cdba981a
Sergey-cyber980 <stalker_angel@mail.ru>
2024-11-11 20:45:34 +0300

    Д/з по теме "Файлы в операционной системе".

--- module_7_5.py
+++ module_7_5.py
@@ -5,18 +5,14 @@ directory = "."
 
 for root, dirs, files in os.walk(directory):
     for file in files:
-        filepath = os.path.join(root, file) # Формируем полный путь к файлу
+        filepath = os.path.join(root, file) 
+        
+        filetime = os.path.getmtime(filepath) 
 
-        filetime = os.path.getmtime(filepath) # Получаем время последнего изменения файла
-
-        # Форматируем время
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
 
-        # Получаем размер файла
         filesize = os.path.getsize(filepath)
 
-        # Получаем родительскую директорию файла
         parent_dir = os.path.dirname(filepath)
 
-        # Выводим информацию о файле
         print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time}, Родительская директория: {parent_dir}')
