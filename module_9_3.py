ea2794ad560bf64f25e584cf61c7696479e7014e
Sergey-cyber980 <stalker_angel@mail.ru>
2024-11-23 16:32:17 +0300

    Д/з по теме "Генераторные сборки"

---
+++ module_9_3.py
@@ -0,0 +1,13 @@
+# Данные списки
+first = ['Strings', 'Student', 'Computers']
+second = ['Строка', 'Урбан', 'Компьютер']
+
+# 1. Генераторная сборка для разницы длин строк, если длины не равны
+first_result = (len(f) - len(s) for f, s in zip(first, second) if len(f) != len(s))
+
+# 2. Генераторная сборка для сравнения длин строк в одинаковых позициях
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+
+# Пример выполнения кода:
+print(list(first_result))
+print(list(second_result))
