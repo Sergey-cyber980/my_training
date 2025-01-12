a17eb317290320d6b3934ab698c3620b8ca7a855
Sergey-cyber980 <stalker_angel@mail.ru>
2025-01-12 18:50:20 +0300

    Домашнее задание по теме "Машина состояний".

---
+++ module_13_4.py
@@ -0,0 +1,67 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+import asyncio
+
+from Bubuka import Duduka
+
+bot = Bot(token=Duduka)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print("Привет! Я бот помогающий твоему здоровью.")
+    await message.answer("Привет! Я бот помогающий твоему здоровью. "
+                         "Формула подсчёта калорий для мужчин"
+                         "Введите команду 'Calories', чтобы рассчитать норму калорий.")
+
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+
+@dp.message_handler(text="Calories")
+async def set_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+
+@dp.message_handler(state=UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=int(message.text))
+    await message.answer(f"Введите свой рост(в см):")
+    await UserState.growth.set()
+
+
+@dp.message_handler(state=UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth=int(message.text))
+    await message.answer(f"Введите свой вес (в кг):")
+    await UserState.weight.set()
+
+
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=int(message.text))
+    data = await state.get_data()
+    age = data['age']
+    growth = data['growth']
+    weight = data['weight']
+
+    bmr = (10 * weight) + (6.25 * growth) - (5 * age) + 5
+    await message.answer(f"Ваша норма калорий: {bmr} ккал")
+    await state.finish()
+
+
+@dp.message_handler()
+async def all_massages(message):
+    print("Введите команду /start, чтобы начать общение.")
+    await message.answer("Введите команду /start, чтобы начать общение.")
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
